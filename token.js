53a85cdeba8084ed887c138a5855614447eaae42
AfganOFC <afgan.smpn18@gmail.com>
2025-02-08 11:28:06 +0700

    Menambahkan token 7265977269:AAHmv4bCMQljRx3rSnkdG13CJvG6O_zTd3I

--- token.js
+++ token.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "6775449961:AAHpNrhUzGShXacihQCwIkRAwUgZ4UDA2ZY"
+    "6775449961:AAHpNrhUzGShXacihQCwIkRAwUgZ4UDA2ZY",
+    "7265977269:AAHmv4bCMQljRx3rSnkdG13CJvG6O_zTd3I"
   ]
 }
